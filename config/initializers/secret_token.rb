30916581ca32356650990d42b72f0c7590e19e65
Saurabh Agrawal <saurabh@chronus.com>
2012-10-28 01:51:05 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qaapp::Application.config.secret_token = '6bf0ac27a62a543e3196332ed1e3ba12896683ac0846312c99f2622f6737727d7fefb957294dad86893dcdc86f734e9cee865e1ed131f7567b94f07b75d50117'
